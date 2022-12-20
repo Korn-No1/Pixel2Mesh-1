b96e5d8a8fd53481c2ff2e1b31cab07c14d5bc15
Keyue <keyue.zhang@foxmail.com>
2022-12-20 13:52:24 +0100

    12.20

--- utils/average_meter.py
+++ utils/average_meter.py
@@ -18,10 +18,11 @@ class AverageMeter(object):
         self.avg = 0
         self.sum = 0
         self.count = 0
-
+    #更新值 更具val的类型
     def update(self, val, n=1):
         if isinstance(val, torch.Tensor):
             val = val.cpu().numpy()
+            #torch.tensor转换为ndarray
         if isinstance(val, Iterable):
             val = np.array(val)
             self.update(np.mean(np.array(val)), n=val.size)
@@ -33,3 +34,7 @@ class AverageMeter(object):
 
     def __str__(self):
         return "%.6f (%.6f)" % (self.val, self.avg)
+
+
+#计算平均值 print可以返回当前平均值？和历史平均值
+#用法AverageMeter.update(val,n=?)
